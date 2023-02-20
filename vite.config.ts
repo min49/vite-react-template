1e066e6bfe98629dabe605eaf9de218030c91dd7
Min Thu Aung <reoknite@gmail.com>
2023-02-19 22:45:16 -0600

    add vite-plugin-eslint to fail vite on eslint error.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({mode}) => {
+  const conditionalPlugins = mode === 'test' ? [] : [eslint()]
+
+  return {
+    plugins: [react(), tsconfigPaths(), ...conditionalPlugins],
+  }
 })
